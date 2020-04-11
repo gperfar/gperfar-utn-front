57a40c486e0691a4732fb5bb9eea799d986c2ea1
PABLO ISRAEL <gperfar@gmail.com>
2020-04-11 15:19:45 -0300

    installed material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           I'm the best front-end developer in the world.
         </p>
+        <Button variant="contained" color="primary">Do nothing</Button>
       </div>
     );
   }
