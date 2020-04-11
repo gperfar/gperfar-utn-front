ba118e6a668893a6f880e32e5d93c360fb65bf01
PABLO ISRAEL <gperfar@gmail.com>
2020-04-11 14:54:41 -0300

    funny first message

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Simplified Data Analysis</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          I'm the best front-end developer in the world.
         </p>
       </div>
     );
