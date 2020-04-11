0c73455e8ea2bde9eb9b6cb807fa942325fc7f10
PABLO ISRAEL <gperfar@gmail.com>
2020-04-11 16:47:49 -0300

    Revert "Installed react-toolbox"
    This reverts commit 1b1758d6b0c5b73b783018ca8644416b0114dcfd.
    
    dfasd
    
    	modified:   src/App.js
    dfdasdf:w
    :W
    W:

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
-import { styled } from '@material-ui/core/styles';
-
-
-const CoolButton = styled(Button)({
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
-  height: 48,
-  padding: '0 30px',
-});
-
 
 class App extends Component {
   render() {
@@ -27,10 +13,6 @@ class App extends Component {
         <p className="App-intro">
           I'm the best front-end developer in the world.
         </p>
-        <p>
-          However, I don't know how any of this works, so even though I can show a very cool looking button, I don't know how to do something with it
-        </p>
-        <CoolButton variant="contained" color="primary">Do nothing</CoolButton>
       </div>
     );
   }
